8b56421725f7916c0257151a80730b27837f0689
teasleepy <ameena07@gmail.com>
2022-02-25 18:21:23 -0800

    object interaction
    star disappears after touched by player and the star count goes up by 1

---
+++ JarofStars/Assets/Scripts/PickUp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //verify if colliding with player
+        PlayerManager manager = collision.GetComponent<PlayerManager>();
+        if(manager)
+        {
+            manager.PickupItem();
+
+            //star will dissapear once touched by player
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ JarofStars/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int starCount;
+
+    public void PickupItem()
+    {
+        starCount++;
+
+    }
+}
