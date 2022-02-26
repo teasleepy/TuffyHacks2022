586baf8ca8187c08085d343d1014bd1f62b899f7
teasleepy <ameena07@gmail.com>
2022-02-25 17:39:45 -0800

    project start
    very basic first steps
    
    Player movement and etc

---
+++ JarofStars/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    //check for arrow inputs
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    }
+}
