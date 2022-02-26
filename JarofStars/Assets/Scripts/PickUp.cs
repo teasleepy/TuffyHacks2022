7880f5215f5196c266fc2a7eb77332af7242e28b
teasleepy <ameena07@gmail.com>
2022-02-26 13:49:23 -0800

    cleaned up code a bit
    Cleaned up the code a little bit and added in some more for future stuff in the comments

--- JarofStars/Assets/Scripts/PickUp.cs
+++ JarofStars/Assets/Scripts/PickUp.cs
@@ -16,9 +16,10 @@ public class PickUp : MonoBehaviour
 
             //star will be reset once touched by player
 
+            //so that stars won't appear in same position every time 
             float randomNumber = Random.Range(-8, 8);
 
-            transform.position = new Vector2(randomNumber, 7);
+            transform.position = new Vector2(randomNumber, 7); //FOR FUTURE change y position to random too?
 
             //reset velocity (of the falling star) to 0 
             rb = GetComponent<Rigidbody2D>();
